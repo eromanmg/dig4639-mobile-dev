b3996b319e7062a9c309153b0212f3a321c263f5
Edequiel Roman <ed838227@ucf.edu>
2020-02-03 10:14:52 -0500

    commit

--- ice/w5a/src/index.js
+++ ice/w5a/src/index.js
@@ -11,12 +11,23 @@ function runOnLoad()
     var addTaskButton = document.getElementById("addTask");
     addTaskButton.addEventListener("click", onClick)
     var taskInput = document.getElementById("taskText");
+}
     
 function onClick() {
     console.log("clicked!");
     let newTask = document.createElement("div");
-
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    let span = document.createElement("span");
+    span.innerHTML = taskInput.value;
+    newTask.appendChild(input);
+    newTask.appendChild(span);
+    function onChange() {
+        console.log(span.innerHTML);
+    }
+    input.addEventListener("change", onChange);
     newTask.innerHTML = `<input type='checkbox'></input><span>${taskInput.value}</span>`;
+    
     element.appendChild(newTask);
 }
 
