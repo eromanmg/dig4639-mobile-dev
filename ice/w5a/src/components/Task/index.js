8f885b4d490b02632f1c763811c4665375a7355e
eromanmg <eromanrd@knights.ucf.edu>
2020-02-07 11:34:01 -0500

    commit

--- ice/w5a/src/components/Task/index.js
+++ ice/w5a/src/components/Task/index.js
@@ -7,31 +7,14 @@ class Task {
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         this.onClick = this.onClick.bind(this);
-<<<<<<< HEAD
-        checkbox.addEventListener("change", this.onClick)
-=======
         checkbox.addEventListener("change",this.onClick);
 
->>>>>>> f5bd81462727dd54f2ab2a0e49a690edec7ebe49
         this.element.appendChild(checkbox);
         let span = document.createElement("span");
         this.element.appendChild(span);
         span.innerHTML = this.props.content;
         this.element.className = "task";
     }
-<<<<<<< HEAD
-    onClick(e){
-        console.log(this);
-        console.log(e.target);
-        console.log(e.target.checked);
-
-    }
-render() {
-
-    return this.element;
-  }
-}
-=======
     onClick(e) {
         console.log(this);
         console.log(e.target);
@@ -42,5 +25,4 @@ render() {
     }
 }
 
->>>>>>> f5bd81462727dd54f2ab2a0e49a690edec7ebe49
 export default Task;
