54f59e94ec67e4ba3ceed25ba01f8e7fe575da0f
eromanmg <eromanrd@knights.ucf.edu>
2020-02-27 12:13:08 -0500

    commit

--- labs/3/my-react-app/src/App.js
+++ labs/3/my-react-app/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from'./components/card/index.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  myArray = [{v:"MyString",t:"Title"},{v:"Another String"},{v:"String 5"}]
+  myFunction() {
+    console.log("Hi there");
+  }
+  render () {
+    return (
+      <>
+ 
+      <Card content="This is a card!" myFunction={() => this.myFunction()}></Card>
+      </>
+    );
+  }
 }
 
 export default App;
--- labs/3/my-react-app/src/components/card/index.js
+++ labs/3/my-react-app/src/components/card/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './index.css';
 
 class Card extends React.Component{
-    consructor(props) {
-        this.props = props;
-        this.element = document.createElement("div");
-        this.element.innerHTML = this.props.content;
-        let span = document.createElement("span");
-        this.element.appendChild(span);
-        span.innerHTML = this.props.content;
-        this.element.className = "card";
+    constructor(props) {
+        super(props)
+        // this.props = props;
+        // this.element = document.createElement("div");
+        // this.element.innerHTML = this.props.content;
+        // let span = document.createElement("span");
+        // this.element.appendChild(span);
+        // span.innerHTML = this.props.content;
+        // this.element.className = "card";
     }
     render() {
-        return this.element;
+        return <p className="card">{this.props.content}</p>;
+    }
 }
 
 export default Card;
-
-
